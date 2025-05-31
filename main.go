5261284ca027e485196c75993d6ea57553dcfde3
jalal-codr <abduljalal849@gmail.com>
2025-05-31 08:25:21 +0100

    Refactor periodic request interval to 9 minutes and remove dotenv loading logic

--- main.go
+++ main.go
@@ -5,10 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"time"
-
-	"github.com/joho/godotenv"
 )
 
 // MakeGetRequest makes an HTTP GET request to the given URL and returns the response body or an error.
@@ -45,18 +42,12 @@ func StartServer() {
 }
 
 func main() {
-	if os.Getenv("RENDER") == "" { // Render sets RENDER env variable in production
-		err := godotenv.Load()
-		if err != nil {
-			log.Println("No .env file found, running without .env")
-		}
-	}
 
 	// Example URL to send the GET request to
 	url := "https://tradingbot-2w81.onrender.com/"
 
-	// Create a ticker that ticks every 10 minutes
-	ticker := time.NewTicker(10 * time.Minute)
+	// Create a ticker that ticks every 9 minutes
+	ticker := time.NewTicker(9 * time.Minute)
 	defer ticker.Stop()
 
 	// Start the server in a separate goroutine
@@ -64,33 +55,30 @@ func main() {
 
 	// Initialize lastRequestTime with the current time
 	lastRequestTime := time.Now()
-	log.Println("Starting periodic requests. First request will be made in 10 minutes.")
+	log.Println("Starting periodic requests. First request will be made in 9 minutes.")
 
-	// Run the GET request every 10 minutes in the background
-	for {
-		select {
-		case <-ticker.C:
-			// Calculate and log the time since the last request
-			currentTime := time.Now()
-			elapsedTime := currentTime.Sub(lastRequestTime)
-			log.Printf("Time since last request: %v", elapsedTime)
+	// Run the GET request every 9 minutes in the background
+	for range ticker.C {
+		// Calculate and log the time since the last request
+		currentTime := time.Now()
+		elapsedTime := currentTime.Sub(lastRequestTime)
+		log.Printf("Time since last request: %v", elapsedTime)
 
-			// Update lastRequestTime to the current time
-			lastRequestTime = currentTime
+		// Update lastRequestTime to the current time
+		lastRequestTime = currentTime
 
-			// Log that we're making a request
-			log.Printf("Making GET request to %s", url)
+		// Log that we're making a request
+		log.Printf("Making GET request to %s", url)
 
-			// Make the request
-			body, err := MakeGetRequest(url)
-			if err != nil {
-				log.Println("Error:", err)
-				continue
-			}
-
-			// Print the response body
-			log.Println("Response received at:", currentTime.Format(time.RFC3339))
-			log.Println("Response Body:", body)
+		// Make the request
+		body, err := MakeGetRequest(url)
+		if err != nil {
+			log.Println("Error:", err)
+			continue
 		}
+
+		// Print the response body
+		log.Println("Response received at:", currentTime.Format(time.RFC3339))
+		log.Println("Response Body:", body)
 	}
 }
